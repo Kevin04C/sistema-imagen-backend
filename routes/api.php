eb96722b4f70352a47f8ecb22bc0f90988762ca9
Kevin04c <kevincespedes2004@gmail.com>
2023-10-28 16:06:39 -0500

    Update api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,9 +24,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/users', [UserController::class, 'getUsers']);
 Route::get('/users/{id}', [UserController::class, 'getUser']);
+Route::put("users/{id}", [UserController::class, "updateUser"]);
+
+Route::get('/roles', [RoleController::class, 'getRoles']);
 
 Route::post("/auth/register", [AuthController::class, 'register']);
 Route::post("/auth/login", [AuthController::class, 'login']);
 Route::get("/auth/renew", [AuthController::class, 'renewToken']);
 
+
 Route::post('/products', [ProductController::class, 'createProduct']);
+    
